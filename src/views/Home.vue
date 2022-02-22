d92a8c8c7cdec07e34e12121b8bce15b47d21ab9
Luis Antonio Parrado <luisprmat@gmail.com>
2022-02-22 00:06:30 -0500

    Install and configure bootstrap sass

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+
+    <h1>Hola mundo</h1>
+
+    <button class="btn btn-secondary">Cancelar</button>
+    <button class="btn btn-primary">Aceptar</button>
   </div>
 </template>
