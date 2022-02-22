07f1db941ab77227449413171c1f4a88d8e88c00
Luis Antonio Parrado <luisprmat@gmail.com>
2022-02-22 00:44:49 -0500

    Create modular structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
+import dayBookRouter from '@/modules/daybook/router'
+
 const routes = [
   {
     path: '/',
@@ -10,10 +12,11 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/daybook',
+    ...dayBookRouter
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,18 @@
 
     <h1>Hola mundo</h1>
 
-    <button class="btn btn-secondary">Cancelar</button>
-    <button class="btn btn-primary">Aceptar</button>
+    <button @click="goToDaybook" class="btn btn-secondary">Cancelar</button>
+    <button @click="goToDaybook" class="btn btn-primary">Aceptar</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    goToDaybook() {
+      this.$router.push({ name: 'daybook' })
+    }
+  }
+}
+</script>
+
