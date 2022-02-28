d6571cb88e9956b7b405dff5568da03fb68382da
Luis Antonio Parrado <luisprmat@gmail.com>
2022-02-27 23:27:30 -0500

    Journal -  create Vuex module

---
+++ src/modules/daybook/store/journal/getters.js
@@ -0,0 +1,11 @@
+// export const myGetter = (state) => {
+//   return state.some
+// }
+
+export const getEntriesByTerm = ( /*state*/ ) => {
+  
+}
+
+export const getEntryById = ( /*state*/ ) => {
+  
+}
