73d7d0ff9d644e721bb04d5b1f328c0177a59e65
Luis Antonio Parrado <luisprmat@gmail.com>
2022-02-28 23:09:17 -0500

    Show entry or redirect user (use a helper)

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="entry-title d-flex justify-content-between p-2">
     <div>
-      <span class="text-success fs-3 fw-bold">27</span>
-      <span class="mx-1 fs-3">Febrero</span>
-      <span class="mx-2 fs-4 fw-light">2022, domingo</span>
+      <span class="text-success fs-3 fw-bold">{{ day }}</span>
+      <span class="mx-1 fs-3">{{ month }}</span>
+      <span class="mx-2 fs-4 fw-light">{{ yearDay }}</span>
     </div>
 
     <div>
@@ -23,6 +23,7 @@
 
   <div class="d-flex flex-column px-3 h-75">
     <textarea
+      v-model="entry.text"
       placeholder="¿Qué sucedió hoy?"
     ></textarea>
   </div>
@@ -40,6 +41,8 @@
 import { defineAsyncComponent } from 'vue'
 import { mapGetters } from 'vuex' // computed!!!
 
+import getDayMonthYear from '../helpers/getDayMonthYear'
+
 export default {
   props: {
     id: {
@@ -50,13 +53,32 @@ export default {
   components: {
     Fab: defineAsyncComponent(() => import(/* webpackChunkName: "fab" */ '../components/Fab'))
   },
+  data() {
+    return {
+      entry: null
+    }
+  },
   computed: {
-    ...mapGetters('journal', ['getEntryById'])
+    ...mapGetters('journal', ['getEntryById']),
+    day() {
+      const { day } = getDayMonthYear( this.entry.date )
+      return day
+    },
+    month() {
+      const { month } = getDayMonthYear( this.entry.date )
+      return month
+    },
+    yearDay() {
+      const { yearDay } = getDayMonthYear( this.entry.date )
+      return yearDay
+    }
   },
   methods: {
     loadEntry() {
       const entry = this.getEntryById( this.id )
-      console.log(entry)
+      if ( !entry ) this.$router.push({ name: 'no-entry' })
+
+      this.entry = entry
     }
   },
   created() {
