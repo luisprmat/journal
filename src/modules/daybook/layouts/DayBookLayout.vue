538b3860968b5c3703fd9bb038ffcf567625d3a2
Luis Antonio Parrado <luisprmat@gmail.com>
2022-03-01 00:30:27 -0500

    Load entries with axios from backend (firebase)

--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -13,11 +13,18 @@
 
 <script>
 import { defineAsyncComponent } from 'vue'
+import { mapActions } from 'vuex'
 
 export default {
   components: {
     Navbar: defineAsyncComponent(() => import('../components/Navbar')),
     EntryList: defineAsyncComponent(() => import('../components/EntryList')),
+  },
+  methods: {
+    ...mapActions('journal', ['loadEntries'])
+  },
+  created() {
+    this.loadEntries()
   }  
 }
 </script>
--- src/modules/daybook/store/journal/actions.js
+++ src/modules/daybook/store/journal/actions.js
@@ -1,9 +1,12 @@
 // export const myAction = async ({ commit }) => {
 
+import journalApi from "@/api/journalApi"
+
 // }
 
 export const loadEntries = async (/*{ commit }*/) => {
-
+  const { data } = await journalApi.get('/entries.json')
+  console.log(data)
 }
 
 export const updateEntry = async (/*{ commit }*/) => {
