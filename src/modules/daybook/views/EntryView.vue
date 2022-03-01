73d770b132bde12334fbd1bbb11ce19d9fce2519
Luis Antonio Parrado <luisprmat@gmail.com>
2022-02-28 23:26:46 -0500

    Load entry when url changes (use watch)

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -1,40 +1,41 @@
 <template>
-  <div class="entry-title d-flex justify-content-between p-2">
-    <div>
-      <span class="text-success fs-3 fw-bold">{{ day }}</span>
-      <span class="mx-1 fs-3">{{ month }}</span>
-      <span class="mx-2 fs-4 fw-light">{{ yearDay }}</span>
+  <template v-if="entry">
+    <div class="entry-title d-flex justify-content-between p-2">
+      <div>
+        <span class="text-success fs-3 fw-bold">{{ day }}</span>
+        <span class="mx-1 fs-3">{{ month }}</span>
+        <span class="mx-2 fs-4 fw-light">{{ yearDay }}</span>
+      </div>
+
+      <div>
+        <button class="btn btn-danger mx-2">
+          Borrar
+          <i class="fa fa-trash-alt"></i>
+        </button>
+
+        <button class="btn btn-primary">
+          Subir foto
+          <i class="fa fa-upload"></i>
+        </button>
+      </div>
     </div>
 
-    <div>
-      <button class="btn btn-danger mx-2">
-        Borrar
-        <i class="fa fa-trash-alt"></i>
-      </button>
+    <hr>
 
-      <button class="btn btn-primary">
-        Subir foto
-        <i class="fa fa-upload"></i>
-      </button>
+    <div class="d-flex flex-column px-3 h-75">
+      <textarea
+        v-model="entry.text"
+        placeholder="¿Qué sucedió hoy?"
+      ></textarea>
     </div>
-  </div>
 
-  <hr>
-
-  <div class="d-flex flex-column px-3 h-75">
-    <textarea
-      v-model="entry.text"
-      placeholder="¿Qué sucedió hoy?"
-    ></textarea>
-  </div>
+    <img
+      src="https://www.robertlandscapes.com/wp-content/uploads/2018/05/IMG_3064-2-400x300.jpg"
+      alt="entry-picture"
+      class="img-thumbnail">
+  </template>
 
   <Fab icon="fa-save" />
-
-  <img
-    src="https://www.robertlandscapes.com/wp-content/uploads/2018/05/IMG_3064-2-400x300.jpg"
-    alt="entry-picture"
-    class="img-thumbnail">
-
 </template>
 
 <script>
@@ -83,6 +84,11 @@ export default {
   },
   created() {
     this.loadEntry()
+  },
+  watch: {
+    id() {
+      this.loadEntry()
+    }
   }
 }
 </script>
