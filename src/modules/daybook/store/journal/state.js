8ac74b049257f0a80ca4807d61d4a8c578744be2
Luis Antonio Parrado <luisprmat@gmail.com>
2022-03-01 00:48:25 -0500

    Commit a mutation with the data obtained from back

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,23 +1,4 @@
 export default () => ({
   isLoading: true,
-  entries: [
-    {
-      id: '1',
-      date: new Date().toDateString(),
-      text: 'Commodo pariatur elit Lorem consequat dolore. Eu do reprehenderit ex sint officia proident commodo nisi eu consequat. Consequat sint amet eu non eiusmod adipisicing.',
-      picture: null
-    },
-    {
-      id: '2',
-      date: new Date().toDateString(),
-      text: 'Magna nisi laboris nostrud sint anim enim et magna non esse sit. Lorem consectetur veniam deserunt est qui adipisicing labore. Quis nisi aliqua magna cillum eu laboris amet veniam.',
-      picture: null
-    },
-    {
-      id: '3',
-      date: new Date().toDateString(),
-      text: 'Magna ea ipsum fugiat reprehenderit minim. Exercitation labore amet ad id qui commodo est aliquip irure voluptate ullamco duis amet consectetur. Cupidatat laboris laboris labore ex. Minim do fugiat sunt cupidatat cupidatat cupidatat officia duis. Velit ut cupidatat cillum incididunt ut commodo labore deserunt excepteur minim. Eiusmod eu do eiusmod aliqua labore incididunt sint ad dolor amet.',
-      picture: null
-    }
-  ]
+  entries: []
 })
